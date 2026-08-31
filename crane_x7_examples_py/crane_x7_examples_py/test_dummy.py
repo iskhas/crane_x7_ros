ea1f39fe995748ca273792a28e1713969dc5f138
Yuga-soy <soysoyuec25@gmail.com>
2026-08-31 10:47:01 +0900

    colcon testをパスするためのファイルを追加

---
+++ crane_x7_examples_py/crane_x7_examples_py/test_dummy.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestUnittestDummy(unittest.TestCase):
+
+    def test_dummy_method(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
